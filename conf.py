cb2594aaf8f83fd470ec4e6df9801e25312f4673
Joon Ro <joonhyoung.ro@gmail.com>
2013-02-18 23:11:29 -0600

    added option to remove blank pages in pdf
    https://github.com/scipy/scipy/blob/master/scipy/interpolate/rbf.py

--- conf.py
+++ conf.py
@@ -169,7 +169,11 @@ htmlhelp_basename = 'MKT337PrinciplesofMarketingdoc'
 
 # -- Options for LaTeX output --------------------------------------------------
 
-latex_elements = {
+latex_elements = { 
+    'classoptions': ',openany,oneside', 
+    'babel' : '\\usepackage[english]{babel}' }
+
+#latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
@@ -178,7 +182,7 @@ latex_elements = {
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
-}
+#}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
